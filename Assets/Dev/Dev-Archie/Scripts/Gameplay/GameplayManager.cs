abe9c92d42d7c00a97e3a663747b02e1ad29d530
Archie <57532087+Rockblank@users.noreply.github.com>
2026-07-04 00:20:12 +0700

    Padepokan pause

--- Assets/Dev/Dev-Archie/Scripts/Gameplay/GameplayManager.cs
+++ Assets/Dev/Dev-Archie/Scripts/Gameplay/GameplayManager.cs
@@ -35,7 +35,7 @@ namespace ODGJ.Gameplay
             
             // Ambil data Hari ke-X dari PlayerPrefs
             int currentNight = PlayerPrefs.GetInt("CurrentNight", 1);
-            if(txtNight != null) txtNight.text = $"Night: {currentNight}";
+            if(txtNight != null) txtNight.text = currentNight.ToString();
         }
 
         private void OnEnable() {
